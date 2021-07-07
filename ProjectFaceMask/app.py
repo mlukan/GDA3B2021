6a0b7ffbbc0f7b3cdd0a92727157d17a6b0b8cda
mlukan <mlukan@gmail.com>
2021-07-07 09:03:52 +0000

    Added comments to streamlit app

--- ProjectFaceMask/app.py
+++ ProjectFaceMask/app.py
@@ -10,23 +10,27 @@ import io
 import os
 import keras
 import matplotlib.pyplot as plt
+
+# Load local face mask detection model, can be exchanged for a better one :)
 model = keras.models.load_model('facemaskcnn.h5')
+
+# How to run this file:
+# streamlit run app.py
+# on default port, or set the port explicitly such as:
+# streamlit run app.py --server.port 8080
 
 from tempfile import NamedTemporaryFile
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
-
-
-
-
-
+# Cache decorators had to be removed as below,because  streamlit cannot hash keras objects
 #@st.cache(allow_output_mutation=True)
 def highlight_faces(model,image):
     from mtcnn.mtcnn import MTCNN
     import cv2
     from matplotlib.patches import Rectangle
-
+    
+    #---Start embedded functions---
     #@st.cache(allow_output_mutation=True)
     def detect_faces(image):
         im = np.asarray(image)
@@ -37,10 +41,12 @@ def highlight_faces(model,image):
         faces = detector.detect_faces(im)
         return faces
 
-    # for each face, draw a rectangle based on coordinates
+    # Get predictions  for all faces in image, return a list of dictionaries with face locations and predictions
     #@st.cache(allow_output_mutation=True)
     def get_predictions(faces):
         facepredictions,facelist=[],[]
+        
+        # Extract, crop and resize all detected faces
         for face in faces:
             x, y, width, height = face['box']
             im = image
@@ -48,19 +54,28 @@ def highlight_faces(model,image):
             imresized = im.resize((32,32))
             imarray=np.array(imresized)[...,:3]
             facelist.append(imarray)
+            # Save face location metadata for later use
             facepredictions.append(dict(x=x,y=y,width=width,height=height))
+        
+        # Create a numpy array of all faces for model input    
         facearray=np.stack([np.array(im)[...,:3]  for im in facelist], axis=0)
-        #facearray=np.stack([im for im in facelist], axis=0)
-
+        
+        # Write number of faces on the webpage
         st.write(f'{len(facelist)} faces were found')
+        # Make face mask prediction on the face numpy array input
         predictions = model.predict(facearray/255)
         #st.write('predictions shape',predictions.shape)
         predictions=np.argmax(predictions,axis=1)
         #st.write(predictions)
+        
+        #Append prediction data  to the face metadata list
         for i,face in enumerate(facepredictions):
             facepredictions[i]['prediction'] = predictions[i]
-                    
+        
+        #Return list of image location metadata and predictions            
         return facepredictions
+    
+    # Mark face boundaries on the image and color the rectangles  by prediction
     #@st.cache(allow_output_mutation=True)
     def mark_faces(**fp):
         #Color code for classes
@@ -69,36 +84,43 @@ def highlight_faces(model,image):
         face_border = Rectangle((fp['x']-5, fp['y']-5), fp['width']+10, fp['height']+10,
                           fill=False, color=colors[fp['prediction']])
         ax.add_patch(face_border)
-    #End embedded functions
-    #Main function code
+    #---end of embedded functions---
+    
+    #---Start main function code---
     faces=detect_faces(image)    
-    # display base image
-    #img = plt.imread(image)
+    
+    # Display base image
     img = plt.imshow(image)
 
     ax = plt.gca()
     # Get predictions
     predictions=get_predictions(faces)
     
-    # Mark faces by prediction
+    # Plot predicted face boundaries  over the base image
     for pred in predictions:
         mark_faces(**pred)
+    
+    # Return plot
     return plt
 
 
 def main():
-
+    # Streamlit header text
     st.header("Face Mask Detection")
     st.subheader("Drag and drop an image with faces.")
+    
+    # Streamlit embedded html code, just used for color styling
     st.markdown("""<p>Face mask classification:</p><span style="color:green;">Correctly worn mask |</span> <span style="color:orange;"> Incorrectly worn mask |</span><span style="color:red;"> Without mask</span>""",unsafe_allow_html=True)
+    
+    # File uploader, load image into teporary buffer
     buffer = st.file_uploader("Upload Image",type=['png','jpeg','jpg'])
     temp_file = NamedTemporaryFile(delete=False)
     if buffer:
         temp_file.write(buffer.getvalue())
-        #st.write(load_img(temp_file.name))
-        #st.write(temp_file.name)        
+        # Open the file from the buffer as PIL image
         image = Image.open(temp_file.name)
-        #st.image(image, caption='test image')
+        
+        # Streamlit call to plot the (pyplot) output of the highlight_faces function
         st.pyplot(highlight_faces(model,image))
 
 if __name__ == "__main__":
